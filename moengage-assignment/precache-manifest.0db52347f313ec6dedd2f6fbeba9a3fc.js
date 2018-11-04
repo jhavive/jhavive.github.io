2186e6843dd0ebe0e0fa7c4b321b1b74668e9e90
Vivek Jha <jhavive@gmail.com>
2018-11-04 17:45:22 +0530

    moengage assignment

---
+++ moengage-assignment/precache-manifest.0db52347f313ec6dedd2f6fbeba9a3fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3dd63780e6155b9dd0d",
+    "url": "/static/js/main.e3dd6378.chunk.js"
+  },
+  {
+    "revision": "77796c0966c8cbe3e29c",
+    "url": "/static/js/1.77796c09.chunk.js"
+  },
+  {
+    "revision": "e3dd63780e6155b9dd0d",
+    "url": "/static/css/main.0425fdb4.chunk.css"
+  },
+  {
+    "revision": "bdfc1edb25f29676cd9237c85495b756",
+    "url": "/index.html"
+  }
+];
